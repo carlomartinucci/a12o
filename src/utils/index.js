867748a03e0d3473e2f448accdf55718722ee2cb
Carlo Martinucci <carlo.martinucci@gmail.com>
2018-10-09 00:12:21 +0200

    f type for Object.values({ [string]: string })

--- src/utils/index.js
+++ src/utils/index.js
@@ -11,8 +11,8 @@ export const randObj = (hash: {}) => {
 }
 
 const currentDay = () => Math.round(Date.now() / (1000 * 60 * 60 * 24))
-export function currentDayObj(hash: {}) {
-  const values = Object.values(hash)
+export function currentDayObj(hash: { [string]: string }): string {
+  const values: Array<string> = (Object.values(hash): any)
   return values[currentDay() % values.length]
 }
 
